2ab37b90ffc0ba30f3ced3cbf1764fc83b9559ff
thomas <koppen191206@gmail.com>
2024-04-02 14:43:02 -0300

    .

--- Restaurante/src/restaurante/Restaurante.java
+++ Restaurante/src/restaurante/Restaurante.java
@@ -19,7 +19,7 @@ import javax.swing.JOptionPane;
     public float salário;
     public String benefícios;
     public String folga;
-    public String horasdetrabalho;
+    public String horas_de_trabalho;
 // const. Funcionário
     Funcionário (String _nome, String _cpf, String _telefone, int _idade){
         super(_nome, _cpf, _telefone, _idade);
@@ -28,9 +28,17 @@ import javax.swing.JOptionPane;
     Funcionário(){}
 }
  class Cliente extends Pessoa{
-    public float valordaconta;
-    public int númerodamesa;
-
+    public float valor_da_conta;
+    public int numero_da_mesa;
+ 
+    public void Mostrar_informacoes () {
+        System.out.println (this.cpf);
+        System.out.println (this.nome);
+        System.out.println (this.telefone);
+        System.out.println (this.numero_da_mesa);
+        System.out.println (this.valor_da_conta);
+    }   
+            
     public Cliente(String _nome, String _cpf, String _telefone, int _idade) {
         super(_nome, _cpf, _telefone, _idade);
     }
@@ -56,15 +64,23 @@ import javax.swing.JOptionPane;
 public class Restaurante{
     public static void main(String[] args) {
     Pessoa T34 = new Pessoa("Carlos ", "123456789 ", "0987654321 ", 30);
-    System.out.println(T34.nome+ T34.cpf+ T34.telefone+ T34.idade );
-    
+   
     String nome_cliente;
     int idade_cliente;
     String telefone_cliente;
     String cpf_cliente;
-    
+   
     nome_cliente =  JOptionPane.showInputDialog ("Digite o nome do cliente");
     telefone_cliente =  JOptionPane.showInputDialog ("Digite o telefone do cliente");
     cpf_cliente =  JOptionPane.showInputDialog ("Digite o cpf do cliente");
+    idade_cliente = Integer.parseInt (JOptionPane.showInputDialog ("Digite a idade do Cliente"));
+    
+   
+    Cliente T25 = new Cliente (nome_cliente, cpf_cliente, telefone_cliente, idade_cliente);
+    T25.Mostrar_informacoes();
+    float valor_da_conta;
+    int numero_da_mesa;
+            
     }
 }
+
